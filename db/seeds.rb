d6234cc3ffd0ae7a4f686cb8196363238edfa8a8
Consuelo Garrido Abedrapo <consuelogarrido.ab@gmail.com>
2021-12-17 08:53:26 -0300

    added admin user to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@mail.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
+
+AdminUser.create!(email: 'consuabed@gmail.com', password: 'ef4200', password_confirmation: 'ef4200') if Rails.env.development?
 
 
 
